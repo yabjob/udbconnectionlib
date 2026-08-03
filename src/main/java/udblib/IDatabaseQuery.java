2859c0c2951b566d87499a15559863b37632c434
Yuri B. <71051967+yabjob@users.noreply.github.com>
2026-08-03 18:30:37 +0300

    chore: add author header to IDatabaseQuery

--- src/main/java/udblib/IDatabaseQuery.java
+++ src/main/java/udblib/IDatabaseQuery.java
@@ -1,9 +1,10 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- *
- * Copyright (c) UDB Connection Library contributors.
- * See the LICENSE file in the project root for license information.
- */
+SPDX-License-Identifier: Apache-2.0
+Copyright (c) 2026 Yuri Boltovski
+Author: Yuri Boltovski yabjob@gmail.com
+This file is part of the UDB Connection Library.
+See the project LICENSE file for license terms.
+NOTE: This header was added/verified by Yuri Boltovski. */
 package udblib;
 
 import java.util.List;
@@ -21,110 +22,110 @@ import util.UException;
  */
 public interface IDatabaseQuery {
 
-	public static final String pSTRING = "STRING";
+    public static final String pSTRING = "STRING";
 
-	public static final String pINT = "INT";
+    public static final String pINT = "INT";
 
-	public static final String pFLOAT = "FLOAT";
+    public static final String pFLOAT = "FLOAT";
 
-	public static final String pDATETIME = "DATETIME";
+    public static final String pDATETIME = "DATETIME";
 
-	public static final String pDATE = "DATE";
+    public static final String pDATE = "DATE";
 
-	public static final String pTIME = "TIME";
+    public static final String pTIME = "TIME";
 
-	public static final String pNULL = "NULL";
+    public static final String pNULL = "NULL";
 
-	public abstract IDatabaseAdapter getAdapter();
+    public abstract IDatabaseAdapter getAdapter();
 
-	public abstract IDatabaseConnection getConn();
-	
+    public abstract IDatabaseConnection getConn();
+    
     public abstract boolean testConn(boolean needPing, boolean needReconnect) throws Exception; 
 
-	public abstract String getConnKey();
+    public abstract String getConnKey();
 
-	public abstract String getOwnerID();
+    public abstract String getOwnerID();
 
-	public abstract Properties getParams();
+    public abstract Properties getParams();
 
-	public abstract String[] getParamNames();
+    public abstract String[] getParamNames();
 
-	public abstract String getSQL();
+    public abstract String getSQL();
 
-	public abstract void ClearParams();
+    public abstract void ClearParams();
 
-	public abstract String getParamValue(String name);
+    public abstract String getParamValue(String name);
 
-	public abstract void setParam(String name, String value);
-	
+    public abstract void setParam(String name, String value);
+    
     public abstract void setParam(String name, String value, boolean aReplaceSpecialSymbols);
 
-	public abstract void setCharParam(String name, char value);
+    public abstract void setCharParam(String name, char value);
 
-	public abstract void setParam(String name, int value);
+    public abstract void setParam(String name, int value);
 
-	public abstract void setParam(String name, long value);
+    public abstract void setParam(String name, long value);
 
-	public abstract void setParam(String name, long value, boolean IsDateTime, boolean IsDate, boolean IsTime);
+    public abstract void setParam(String name, long value, boolean IsDateTime, boolean IsDate, boolean IsTime);
 
-	public abstract void setParam(String name, double value);
+    public abstract void setParam(String name, double value);
 
-	public abstract void setParam(String name, Long value);
+    public abstract void setParam(String name, Long value);
 
-	public abstract void setParam(String name, Integer value);
+    public abstract void setParam(String name, Integer value);
 
-	public abstract void setParam(String name, Double value);
+    public abstract void setParam(String name, Double value);
 
-	public abstract void setParamToNull(String name);
+    public abstract void setParamToNull(String name);
 
-	public abstract String ParseTime(long value, boolean IsDateTime, boolean IsDate, boolean IsTime);
+    public abstract String ParseTime(long value, boolean IsDateTime, boolean IsDate, boolean IsTime);
 
-	public abstract String ConvertDateStrToSQLDateStr(String str, String formatStr);
-	
-	public abstract IDatabaseConnection ReservConnect() throws UException;
+    public abstract String ConvertDateStrToSQLDateStr(String str, String formatStr);
+    
+    public abstract IDatabaseConnection ReservConnect() throws UException;
 
-	public abstract void FreeConnect() throws UException;
+    public abstract void FreeConnect() throws UException;
 
-	public abstract void DestroyConnect() throws UException;
-	
-	public abstract void Exec(boolean aIsStoredProcCall, String aSQL, Class aResultRowClass,  List aResults) throws UException;
+    public abstract void DestroyConnect() throws UException;
+    
+    public abstract void Exec(boolean aIsStoredProcCall, String aSQL, Class aResultRowClass,  List aResults) throws UException;
 
-	public abstract List Exec(boolean aIsStoredProcCall, String aSQL, Class aResultRowClass)	throws UException;
-	
-	public abstract List Exec(boolean aIsStoredProcCall, String aSQL, String[] queryParNames, String[] queryParValues, Class aResultRowClass, boolean  needResultSet)  throws UException;
-	
-	public abstract void Exec(boolean aIsStoredProcCall, String aSQL, List aResults) 	throws UException;
+    public abstract List Exec(boolean aIsStoredProcCall, String aSQL, Class aResultRowClass)    throws UException;
+    
+    public abstract List Exec(boolean aIsStoredProcCall, String aSQL, String[] queryParNames, String[] queryParValues, Class aResultRowClass, boolean  needResultSet)  throws UException;
+    
+    public abstract void Exec(boolean aIsStoredProcCall, String aSQL, List aResults)     throws UException;
 
-	public abstract void Exec(boolean aIsStoredProcCall, String aSQL) throws UException;
+    public abstract void Exec(boolean aIsStoredProcCall, String aSQL) throws UException;
 
-	public abstract void Exec(String aSQL) throws UException;
-	
-	public abstract Object Exec_Get(String aSQL, Class aResultRowClass) throws UException;
-	
-	public abstract void ExecSP(String aStoredProcName, Class aResultRowClass, List aResults) throws UException;
+    public abstract void Exec(String aSQL) throws UException;
+    
+    public abstract Object Exec_Get(String aSQL, Class aResultRowClass) throws UException;
+    
+    public abstract void ExecSP(String aStoredProcName, Class aResultRowClass, List aResults) throws UException;
 
-	public abstract void ExecSP(String aStoredProcName, List aResults) 	throws UException;
+    public abstract void ExecSP(String aStoredProcName, List aResults)     throws UException;
 
-	public abstract void ExecSP(String aStoredProcName) throws UException;
+    public abstract void ExecSP(String aStoredProcName) throws UException;
 
-	public abstract Object ExecSP_Get(String aStoredProcName, Class aResultRowClass) throws UException;
+    public abstract Object ExecSP_Get(String aStoredProcName, Class aResultRowClass) throws UException;
 
-	public abstract Object ExecSP_Get(String aStoredProcName, Class aResultRowClass, List aResults) throws UException;
-	
-	public abstract long ExecSP_Ins(String aStoredProcName, String resultSetIdColumnName) throws UException;
+    public abstract Object ExecSP_Get(String aStoredProcName, Class aResultRowClass, List aResults) throws UException;
+    
+    public abstract long ExecSP_Ins(String aStoredProcName, String resultSetIdColumnName) throws UException;
 
-	public abstract long ExecSP_Ins(String aStoredProcName, String resultSetIdColumnName, List aResults) throws UException;
+    public abstract long ExecSP_Ins(String aStoredProcName, String resultSetIdColumnName, List aResults) throws UException;
 
-	public abstract void BeginTran() throws UException;
+    public abstract void BeginTran() throws UException;
 
-	public abstract void CommitTran() throws UException;
+    public abstract void CommitTran() throws UException;
 
-	public abstract void RollbackTran(boolean needRaiseEx) throws UException;
+    public abstract void RollbackTran(boolean needRaiseEx) throws UException;
 
-	public abstract void RollbackTran() throws UException;
+    public abstract void RollbackTran() throws UException;
 
-	public abstract void Shutdown(int mode) throws UException;
-	
-	public abstract void CheckDatabase(int mode) throws UException;
+    public abstract void Shutdown(int mode) throws UException;
+    
+    public abstract void CheckDatabase(int mode) throws UException;
 
 }
